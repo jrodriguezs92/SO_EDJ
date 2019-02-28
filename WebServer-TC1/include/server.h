cc106d3eeed7fb376468b740d2d8a1cc9eeaf90a
Esteban AP <estape11@gmail.com>
2019-02-28 16:22:51 -0600

    Se corrige la ruta en el instalador y el desinstalar

--- WebServer-TC1/include/server.h
+++ WebServer-TC1/include/server.h
@@ -21,10 +21,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
 #include <fcntl.h>
--- WebServer-TC1/scripts/InstallDaemon.sh
+++ WebServer-TC1/scripts/InstallDaemon.sh
@@ -2,10 +2,10 @@
 
 echo "> Installing WebServer Daemon"
 sudo cp ../bin/WebServer /usr/bin/
-sudo cp webserver.service /usr/lib/systemd/system/
+sudo cp webserver.service /etc/systemd/system/
 sudo mkdir -p /etc/webserver/
 sudo cp webserver.conf /etc/webserver/
 sudo systemctl daemon-reload
-echo "> Complete"
+echo "> Completed"
 echo "Start daemon: sudo systemctl start webserver.service"
 echo "Status daemon: sudo systemctl status webserver.service"
