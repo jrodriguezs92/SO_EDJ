01bc69142860f66bd7f31653d02290b33f816b38
Esteban AP <estape11@gmail.com>
2019-03-01 12:43:40 -0600

    Cambios al desinstalador

--- WebServer-TC1/scripts/UninstallDaemon.sh
+++ WebServer-TC1/scripts/UninstallDaemon.sh
@@ -15,10 +15,12 @@
 
 echo "> Uninstalling WebServer Daemon"
 sudo systemctl stop webserver.service
+sudo systemctl disable webserver.service
 sudo rm -f /usr/bin/WebServer
 sudo rm -f /usr/lib/systemd/system/webserver.service
 sudo rm -rf /etc/webserver/
 sudo rm -f /etc/webserver/webserver.conf
 sudo rm -f /var/log/webserver.log
 sudo systemctl daemon-reload
+sudo systemctl reset-failed
 echo "> Completed"
