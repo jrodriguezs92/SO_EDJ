c906c8d6c9c29df326b4208d9c2dcff9d2f94a67
Esteban AP <estape11@gmail.com>
2019-04-14 21:56:31 -0600

    Se automatiza la instalacion de docker

--- Schedulers-P1/Install.sh
+++ Schedulers-P1/Install.sh
@@ -10,6 +10,38 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
+echo -e '\e[44m>>> VERIFIYNG DOCKER INSTALLATION \e[49m'
+if [ $(which docker | grep -c "/docker") -eq 0 ];
+then
+	echo -e '\e[33m> Installing Docker <\e[39m'
+
+	echo -e '\e[44m>>> UPDATING REPOSITORIES <<< \e[49m'
+	sudo apt-get update -y
+	sudo apt-get install \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg-agent \
+		software-properties-common -y
+
+	echo -e '\e[44m>>> SETTING UP DOCKER REPOSITORY <<< \e[49m'
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo apt-key fingerprint 0EBFCD88 -y
+	sudo add-apt-repository \
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	   $(lsb_release -cs) \
+	   stable" -y
+
+	echo -e '\e[44m>>> UPDATING REPOSITORIES <<< \e[49m'
+	sudo apt-get update -y
+
+	echo -e '\e[44m>>> INSTALLING DOCKER <<< \e[49m'
+	sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+	echo -e '\e[33m> Completed <\e[39m'
+else
+	echo -e '\e[33m> Docker already installed <\e[39m'
+fi
 
 ################# FIFO SERVER
 
