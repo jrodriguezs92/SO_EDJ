a90c1beaa6520aebcaeaa049d9d2fc7a87fea767
Esteban AP <estape11@gmail.com>
2019-05-30 05:53:49 -0600

    Se agrega el proyecto de arduino

---
+++ Checkers-P2/meArm/meArm.ino
@@ -0,0 +1,72 @@
+// ********************************************************
+//            Instituto Tecnológico de Costa Rica
+//              Computer Engineering
+//
+//      Programmer: Esteban Agüero Pérez (estape11)
+//      Programming Language: C / C++
+//      Version: 1.0
+//      Last Update: 28/05/2019
+//
+//              Operating Systems Principles
+//              Professor. Diego Vargas
+// ********************************************************
+
+#include "meArm.h"
+#include <Servo.h>
+
+int baseServo = 11;
+int rightServo = 10;
+int leftServo = 9;
+int clawPin = 8;
+
+meArm arm;
+bool claw = false; // closed
+
+void refresh(String cmd){
+    short value = cmd.substring(1, cmd.length()-1).toInt();
+    if (cmd[0] == 'X') {
+        arm.gotoPoint(value, arm.getY(), arm.getZ());
+           
+    } else if (cmd[0] == 'Y') {
+        arm.gotoPoint(arm.getX(), value, arm.getZ());
+           
+    } else if (cmd[0] == 'Z') {
+        arm.gotoPoint(arm.getX(), arm.getY(), value);
+           
+    } else if (cmd[0] == 'C') {
+        if (claw) {
+            //Serial.println("Closing claw");
+            arm.closeGripper();
+            claw = false;
+            
+        } else {
+            //Serial.println("Opening claw");
+            arm.openGripper();
+            claw = true;
+                
+        }
+        
+    }
+    
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println(F(">> Setting up meArm"));
+    arm.begin(baseServo, rightServo, leftServo, clawPin); // starts the servos
+    arm.gotoPoint(0,150, 50); // initial position
+    
+}
+
+void loop() {
+    //arm.gotoPoint(arm.getX(),arm.getY(),200);
+    //delay(2000);
+    //arm.gotoPoint(0,150, 0);
+    //delay(2000);
+    if (Serial.available() > 0) {
+        refresh(Serial.readString());
+        
+    }
+    delay(500);
+    
+}
