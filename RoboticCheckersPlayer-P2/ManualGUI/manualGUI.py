c7e588425333a41e9ade541b0af7d75baec2a614
DaniHdez <dannyha07@gmail.com>
2019-05-30 12:14:43 -0600

    Changes to GUI background

--- RoboticCheckersPlayer-P2/ManualGUI/manualGUI.py
+++ RoboticCheckersPlayer-P2/ManualGUI/manualGUI.py
@@ -46,14 +46,18 @@ window.title("Manual Interaction GUI - EDJ")
 window.geometry("400x200")
 
 #Canvas
-canvas = Canvas(window, width = 400, height = 200, background = "blue")
+canvas = Canvas(window, width = 400, height = 200)
 canvas.pack()
 
+#Image
+bgImage = PhotoImage(file = "imagenes/bg.gif")
+canvas.create_image(0, 0 , anchor = NW, image = bgImage)
+
 #Title label 
-labelTitle = Label(canvas, text = "Manual Interaction GUI", font = 40, background = "blue")
-labelTitle.place (x = 100, y = 1)
-labelAction = Label(canvas, text = "Set position:", font = 10, background = "blue")
-labelAction.place (x = 10, y = 50)
+labelTitle = Label(canvas, compound = CENTER, text = "Manual Interaction GUI", foreground = "#68B0E0", font = " Times 15 bold italic", background = "#106095")
+labelTitle.place (x = 90, y = 5)
+labelAction = Label(canvas, text = "Set position:", foreground = "#68B0E0", font = "Times 13 italic", background = "#054583")
+labelAction.place (x = 15, y = 50)
 
 #Combobox
 position = Combobox(canvas, values = ["0.0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "3.0", "3.1", "3.2", "3.3", "3.4", "3.5", "3.6", "3.7", "3.8", "3.9", "4.0", "4.1", "4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "5.0", "5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "6.0", "6.1", "6.2", "6.3", "6.4", "6.5", "6.6", "6.7", "6.8", "6.9", "7.0", "7.1", "7.2", "7.3", "7.4", "7.5", "7.6", "7.7", "7.8", "7.9", "8.0", "8.1", "8.2", "8.3", "8.4", "8.5", "8.6", "8.7", "8.8", "8.9", "9.0", "9.1", "9.2", "9.3", "9.4", "9.5", "9.6", "9.7", "9.8", "9.9"])
